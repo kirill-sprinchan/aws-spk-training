f46b4f4a5516f0c5f2a5e46d7186ccb94dc1321f
sprinchanka <kirill.sprinchan@oxagile.com>
2020-01-15 23:14:53 +0300

    Initial commit

---
+++ src/main/java/spk/aws/training/AwsSpkTrainingApplication.java
@@ -0,0 +1,13 @@
+package spk.aws.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpkTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpkTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spk/aws/training/awsspktraining/AwsSpkTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package spk.aws.training.awsspktraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpkTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
